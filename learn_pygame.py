c8a0d42a8db60820ea5fc9a122a51516a2484eee
nave <evangarrison1@gmail.com>
2023-10-20 22:33:56 -0500

    Updates to test game

--- learn_pygame.py
+++ learn_pygame.py
@@ -1,9 +1,14 @@
+"""Importing pygame and sys"""
+import sys
 import pygame
 from pygame.locals import *
-import sys
 
 pygame.init()
 
+# (400, 600) is the size of the window, and the maximum x and y coordinates
+DISPLAYSURF = pygame.display.set_mode((400, 600))
+
+# Why is the pygame.display.update() not in the for loop?
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
